4b5d8181fc42560d3bc75fcf9126d7b262509a83
Grigory Vasilev <grishavasilev62@gmail.com>
2023-04-13 20:33:51 +0300

    add player class

---
+++ TicTacToe/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace TicTacToe
+{
+	// Класс игрока.
+	class Player
+	{
+		// Имя игрока.
+		public string Name;
+
+		// Игрок готов.
+		public bool IsReady;
+
+		// Количество побед.
+		public int WinCount;
+
+		// Количество поражений.
+		public int LoseCount;
+
+		// Конструктор.
+		public Player(string Name, bool IsReady)
+		{
+			this.Name = Name;
+			this.IsReady = IsReady;
+		}
+	}
+}
